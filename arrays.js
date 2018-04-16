092ea8f084e2ffab2889828a6ab351a85974c9cf
Natalie Halloran <natalie.halloran@yahoo.com>
2018-04-16 22:28:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,30 @@ function destructivelyAddElementToBeginningOfArray(array,element) {
   array = [element,...array]
   return array
 }
+function addElementToEndOfArray(array,element) {
+  var newArray = [...array,element]
+  return newArray
+}
+function destructivelyAddElementToEndOfArray(array,element) {
+  array = [...array,element]
+  return array
+}
+function accessElementInArray(array,index) {
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array) {
+  var newArray = array.slice(1)
+  return newArray
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array) {
+  var newArray = array.slice(0,array.length - 1)
+  return newArray
+}
